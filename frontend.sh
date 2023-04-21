0d78909c0daa25d7f3cb5f0fa34f02ddb55d1387
C SURENDRA BABU <surendrababu.c01@gmail.com>
2023-04-21 11:27:48 +0530

    Adding all scripts

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,24 @@
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Installing nginx WebServer<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 yum install nginx -y
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Enabling nginx Service <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl enable nginx
-systemctl start nginx
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Restarting nginx Service <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+systemctl restart nginx
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Copy nginx Reverse Proxy Conf file <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Removing default content from nginx server <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Downloading the frontend content <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Extracting the frontend content <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Restarting nginx Service <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl restart nginx
--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,14 @@
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Copying the mongo repo <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Installing mongodb <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 yum install mongodb-org -y
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Updating listen address from 127.0.0.1 to 0.0.0.0 <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Enabling mongodb Service <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl enable mongod
-systemctl start mongod
 
-## address need to change from 127.0.0.0 to 0.0.0.0
-
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Restarting mongodb Service <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl restart mongod
