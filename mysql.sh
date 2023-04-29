1ad42c0f1062117c2d2511e7b893bce1623b43fe
C SURENDRA BABU <surendrababu.c01@gmail.com>
2023-04-29 17:35:46 +0530

    Adding all scripts

--- mysql.sh
+++ mysql.sh
@@ -1,9 +1,10 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${ script_path }/common.sh
+mysql_root_password=$1
 
 if [ -z "$mysql_root_password" ]; then
-  echo "Input Mysql Root Password Missing"
+  echo Input Mysql Root Password Missing
   exit
 fi
 
