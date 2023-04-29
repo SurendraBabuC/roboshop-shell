7452b3d2a9771b1ff35b11cd425e05704849697e
C SURENDRA BABU <surendrababu.c01@gmail.com>
2023-04-29 17:59:31 +0530

    Adding all scripts

--- payment.sh
+++ payment.sh
@@ -4,7 +4,7 @@ source ${script_path}/common.sh
 rabbitmq_appuser_password=$1
 
 if [ -z "$rabbitmq_appuser_password" ]; then
-  echo "Input Roboshop Appuser Password misssing"
+  echo Input Roboshop Appuser Password misssing
   exit
 fi
 
