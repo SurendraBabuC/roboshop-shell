26235c409b5005b1d1624a501a6ba94ffd9974a1
C SURENDRA BABU <surendrababu.c01@gmail.com>
2023-04-29 17:52:15 +0530

    Adding all scripts

--- shipping.sh
+++ shipping.sh
@@ -3,11 +3,12 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysql_root_password=$1
 
-if [ -z "$mysql_root_password"]; then
-  echo "Input Mysql Root Password misssing"
+if [ -z "$mysql_root_password" ]; then
+  echo Input Mysql Root Password missing
   exit
 fi
 
 component="shipping"
 schema_setup="mysql"
+
 func_java
